4dfb2f20e188cb96d84f18679ea801fc5ac03356
Urtzi Gurkewicz <urtzigurkewicz@outlook.com>
2025-05-19 21:42:52 -0300

    Add files via upload
    Atividade incompleta do Trapézio.

---
+++ Trapezio.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Trapezio {
+
+    public static Scanner tecladoScanner = new Scanner(System.in);
+
+    public static void imprimir(String texto) {
+        System.out.println(texto);
+
+    }
+
+    public static String lerTexto() {
+        String texto;
+        texto = tecladoScanner.nextString();
+        return texto;
+
+        public static double calcularAreaTrapezio(double baseMaior, double baseMenor, double altura) {
+            double 
+        } 
+
+        public static 
+    
+    }
+
+}
+    
