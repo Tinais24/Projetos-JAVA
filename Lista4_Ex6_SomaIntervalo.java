2aab63db43e00a60d44b56b5890dc9b36980eed7
Urtzi Gurkewicz <urtzigurkewicz@outlook.com>
2025-05-21 20:41:12 -0300

    Add files via upload
    Exercício número 6 incompleto de repetição da lista 4.

---
+++ Lista4_Ex6_SomaIntervalo.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+// 6. Faça um algoritmo que imprima a soma dos valores de um intervalo definido pelo usuário.
+
+public class Lista4_Ex6_SomaIntervalo {
+    
+    final static Scanner = new Scanner(System.in);
+
+    
+}
